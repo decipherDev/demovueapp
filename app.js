4e38acac7a6d406cb63b50bb00fef9ad36f9001b
umesht <umesht@localhost.localdomain>
2019-01-20 22:51:20 +0530

    noob commit

---
+++ app.js
@@ -0,0 +1,33 @@
+new Vue({
+  el:'#vue-app',
+
+  data: {
+    name: "umesh",
+    value: 0,
+    x: 0,
+    y: 0
+  },
+
+  methods: {
+    print: function (keyword) {
+      return "keyword";
+    },
+
+    add: function (num){
+      console.log(this.value+num);
+      this.value = this.value+num;
+    },
+
+    subtract: function (num){
+      this.value-= num;
+    },
+
+    getCordinates: function (event) {
+      console.log(event);
+      this.x = event.x;
+      this.y = event.y;
+    }
+  }
+
+
+});
